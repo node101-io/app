14c921729ac7625b078ea12edeb48263b9df3c06
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-08-07 14:32:11 +0300

    Fix blog design

--- models/blog/functions/getIdentifier.js
+++ models/blog/functions/getIdentifier.js
@@ -3,6 +3,8 @@ module.exports = name => {
     .toString()
     .toLocaleLowerCase()
     .split('ı').join('i').split('ğ').join('g').split('ü').join('u').split('ş').join('s').split('ö').join('o').split('ç').join('c')
-    .split('.').join(' ').split(':').join(' ').split(',').join(' ').split(';').join(' ').split('?').join(' ').split('/').join(' ').split('&').join(' ').split('-').join(' ').split('+').join(' ').split('%').join(' ').split('#').join(' ').split('!').join(' ').split('*').join(' ')
+    .split('.').join(' ').split(':').join(' ').split(',').join(' ').split(';').join(' ').split('?').join(' ').split('/').join(' ').split('&').join(' ')
+    .split('-').join(' ').split('+').join(' ').split('%').join(' ').split('#').join(' ').split('!').join(' ').split('*').join(' ').split('\'').join(' ')
+    .split('$').join(' ').split('€').join(' ').split('₺').join(' ')
     .trim().split(' ').map(each => each.trim()).filter(each => each.length).join('_');
 }
