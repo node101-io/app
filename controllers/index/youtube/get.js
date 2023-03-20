d9d7402413652c3abb75ccebfd3c6a7f3c45fab6
Yunus Gürlek <yunusgurlek@Yunus-MacBook-Pro.local>
2023-03-20 17:50:34 +0100

    Update about_us

--- controllers/index/youtube/get.js
+++ controllers/index/youtube/get.js
@@ -1,7 +1,3 @@
 module.exports = (req, res) => {
-  const id = req.originalUrl.split('?')[0].split('/youtube/')[1];
-
-  if (!id || !id.length) return res.render('index/youtube');
-
-  return res.redirect('/youtube/?id=' + id)
+  return res.render('index/youtube');
 }
